ebcd77d9d1546f6c3b1b906825ada663033b8681
MAliseyko <amaksis@gmail.com>
2018-07-07 12:30:00 +0300

    Fixed react-scripts and linter

--- front-end/config-overrides.js
+++ front-end/config-overrides.js
@@ -1,3 +1,23 @@
-module.exports = function override(config, env) {
+const eslintFormatter = require('react-dev-utils/eslintFormatter');
+const { injectBabelPlugin } = require('react-app-rewired');
+
+module.exports = function override(config) {
+  config = injectBabelPlugin('babel-plugin-styled-components', config);
+
+  config.module.rules.push({
+    test: /\.(js|jsx|mjs)$/,
+    enforce: 'pre',
+    use: [
+      {
+        options: {
+          formatter: eslintFormatter,
+          eslintPath: require.resolve('eslint'),
+
+        },
+        loader: require.resolve('eslint-loader'),
+      },
+    ],
+  });
+
   return config;
 };
