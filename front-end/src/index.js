a94b8d24037587e968be380f01958fea4572364a
Julia Murashko <my_dream13@list.ru>
2018-07-07 18:35:28 +0300

    add react-router

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import BrowserRouter from 'react-router-dom/es/BrowserRouter';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+), document.getElementById('root'));
