3b067093413b805404a7e009d6482f0d5a34dd73
Maxim <max281@icloud.com>
2018-07-07 17:13:59 +0300

    ET3-6 Merge two classes in one(SpringInit BackEndAplication),made @RestController instead of @Controller

--- back-end/src/main/java/com/exadel/team3/backend/controllers/TestRestController.java
+++ back-end/src/main/java/com/exadel/team3/backend/controllers/TestRestController.java
@@ -4,12 +4,13 @@ package com.exadel.team3.backend.controllers;
 import com.exadel.team3.backend.entities.Model;
 import com.exadel.team3.backend.services.ModelService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/app")
 public class TestRestController {
 
@@ -17,36 +18,36 @@ public class TestRestController {
     private ModelService modelService;
 
     @RequestMapping("/models/{name}")
-    @ResponseBody
     public Model getModel(@PathVariable String name){
         return modelService.getModelByName(name);
     }
 
     @RequestMapping("/models")
-    @ResponseBody
     public List<Model> getAllModels(){
      return modelService.getAllModels();
     }
 
     @RequestMapping(value = "/models",method = RequestMethod.POST)
-    @ResponseBody
     public void addModel(@RequestBody Model model){
         modelService.addModel(model);
     }
 
-    @RequestMapping(value = "\\/([-\\w._]+)(\\/[-\\w._]\\?(.+)?)?$/",produces = "text/html")
-    public String getHtml(){
-        return "redirect:/index.html";
+    @RequestMapping(value = "/**",produces = "text/html")
+    public void getHtml(HttpServletResponse response){
+
+        try {
+            response.sendRedirect("/index.html");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @RequestMapping(value = "/models/{name}", method = RequestMethod.PUT)
-    @ResponseBody
     public void updateModel(@RequestBody Model model, @PathVariable String name){
         modelService.updateModel(name,model);
     }
 
     @RequestMapping(value = "/models/{name}", method = RequestMethod.DELETE)
-    @ResponseBody
     public void deleteModel(@PathVariable String name){
         modelService.deleteModel(name);
     }
