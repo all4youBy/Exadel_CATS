9cedf8a5dd656339864543cdab19ad3bdc1452da
Yuliya Adamskaya <yuli.adamskaya@gmail.com>
2018-07-09 16:46:30 +0300

    login Page UI

--- front-end/src/Main/Containers/Main.jsx
+++ front-end/src/Main/Containers/Main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import { Switch, Route, Redirect } from 'react-router-dom';
-import Home from '../../Home/Home';
+import LogIn from '../../Scenes/LogIn/LogIn';
+
 
 class Main extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={Home}/>
+        <Route exact path="/" component={LogIn}/>
         <Redirect to="/"/>
       </Switch>
     );
