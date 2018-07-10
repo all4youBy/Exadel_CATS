b09be02563fe6855be9c0837bfab824a2401b135
Maxim <max281@icloud.com>
2018-07-10 17:41:00 +0300

    ET3-9,ET3-10, made basic security config

--- back-end/src/main/java/com/exadel/team3/backend/BackEndApplication.java
+++ back-end/src/main/java/com/exadel/team3/backend/BackEndApplication.java
@@ -1,23 +1,13 @@
 package com.exadel.team3.backend;
 
-import com.exadel.team3.backend.entities.User;
-import com.exadel.team3.backend.entities.UserEducation;
-import com.exadel.team3.backend.entities.UserRole;
-import com.exadel.team3.backend.services.impl.UserServiceImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BackEndApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
