c656e90e0fb57d0680f481629002c17bb8c0a6cb
Stepan Miakchilo <stepan.miakchilo@gmail.com>
2018-07-11 17:13:05 +0300

    Redesigned Topic entity and Topic DAO routines so that topics are stored  in a plain collection that looks like {id, name, parentId}  but can be  agregated on retrieval as a tree. We can retrieve the full tree or a tree  with a certain id at the root

---
+++ back-end/src/main/java/com/exadel/team3/backend/dao/impl/TopicRepositoryImpl.java
@@ -0,0 +1,52 @@
+package com.exadel.team3.backend.dao.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.GraphLookupOperation;
+import org.springframework.data.mongodb.core.aggregation.MatchOperation;
+import org.springframework.data.mongodb.core.aggregation.TypedAggregation;
+
+import com.exadel.team3.backend.dao.TopicRepositoryAggregation;
+import com.exadel.team3.backend.entities.Topic;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+public class TopicRepositoryImpl implements TopicRepositoryAggregation {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Override
+    public List<Topic> getTopicTree() {
+        return getTopicTree(null);
+    }
+
+    @Override
+    public List<Topic> getTopicTree(String parentId) {
+        GraphLookupOperation graphLookupOperation = GraphLookupOperation.builder()
+                .from("topics")
+                .startWith("parentId")
+                .connectFrom("parentId")
+                .connectTo("_id")
+                .as("parentHierarchy");
+
+        MatchOperation match = null;
+        if (!StringUtils.isEmpty(parentId)) {
+            match = new MatchOperation(
+                    new Criteria().orOperator(
+                            Criteria.where("id").is(parentId),
+                            Criteria.where("parentHierarchy.id").is(parentId)
+                    )
+            );
+        }
+
+        TypedAggregation<Topic> aggregation =
+                match != null
+                  ? TypedAggregation.newAggregation(Topic.class, graphLookupOperation, match)
+                  : TypedAggregation.newAggregation(Topic.class, graphLookupOperation);
+
+        return mongoTemplate.aggregate(aggregation, "topics", Topic.class).getMappedResults();
+    }
+
+}
