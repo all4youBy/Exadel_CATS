0a26648201ed9d7c7c2ae7cfacb2c3de092b8570
Stepan Miakchilo <stepan.miakchilo@gmail.com>
2018-07-18 09:34:29 +0300

    Renamed AnswerChecker to TestChecker and added exception handling

--- back-end/src/main/java/com/exadel/team3/backend/services/impl/TestChecker.java
+++ back-end/src/main/java/com/exadel/team3/backend/services/impl/TestChecker.java
@@ -4,39 +4,58 @@ import com.exadel.team3.backend.entities.Question;
 import com.exadel.team3.backend.entities.QuestionType;
 import com.exadel.team3.backend.entities.QuestionVariant;
 import com.exadel.team3.backend.entities.TestItemStatus;
-import org.apache.commons.validator.routines.IntegerValidator;
+import org.springframework.util.NumberUtils;
 import org.springframework.util.StringUtils;
 
-import java.util.Objects;
 import java.util.Optional;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
-public class AnswerChecker {
-    public static TestItemStatus check(Question question, String answerData) {
+class TestChecker {
+
+    static TestItemStatus check(Question question, String answerData) {
         if (question.getType() == QuestionType.MANUAL_CHECK_TEXT || question.getVariants() == null)
             return TestItemStatus.UNCHECKED;
         if (StringUtils.isEmpty(answerData)) return TestItemStatus.UNANSWERED;
 
         switch (question.getType()) {
             case SINGLE_VARIANT:
-                Integer answer = IntegerValidator.getInstance().validate(answerData);
-                return (answer != null && answer < question.getVariants().size()
-                        && question.getVariants().get(answer).isCorrect())
-                            ? TestItemStatus.RIGHT
-                            : TestItemStatus.WRONG;
+                int answer;
+                try {
+                    answer = Integer.parseInt(answerData);
+                } catch (NumberFormatException nex) {
+                    answer =-1;
+                }
+
+                return (
+                        answer >= 0
+                                && answer < question.getVariants().size()
+                                && question.getVariants().get(answer).isCorrect()
+                )
+                        ? TestItemStatus.RIGHT
+                        : TestItemStatus.WRONG;
 
             case MULTI_VARIANT:
-                Stream<Integer> answerVariants = Pattern.compile("[,;]").splitAsStream(answerData).map(s -> IntegerValidator.getInstance().validate(s)).filter(Objects::nonNull);
+                IntStream answerVariants =
+                        Pattern.compile("[,;]")
+                                .splitAsStream(answerData)
+                                .mapToInt(i -> {try {
+                                    return NumberUtils.parseNumber(i, Integer.class);
+                                } catch (NumberFormatException ex) {
+                                    return -1;
+                                }
+                                })
+                                .filter(i -> i>=0);
                 return (
                         answerVariants.count() ==
-                           question.getVariants().stream().filter(QuestionVariant::isCorrect).count()
-                        && answerVariants.allMatch(
-                                i -> i < question.getVariants().size() && question.getVariants().get(i).isCorrect()
-                            )
+                                question.getVariants().stream().filter(QuestionVariant::isCorrect).count()
+                                && answerVariants.allMatch(
+                                variant -> variant < question.getVariants().size()
+                                        && question.getVariants().get(variant).isCorrect()
                         )
-                            ? TestItemStatus.RIGHT
-                            : TestItemStatus.WRONG;
+                )
+                        ? TestItemStatus.RIGHT
+                        : TestItemStatus.WRONG;
 
             case AUTOCHECK_TEXT:
                 Optional<String> correctAnswer = question.getVariants()
@@ -62,4 +81,5 @@ public class AnswerChecker {
         }
         return TestItemStatus.UNCHECKED;
     }
+
 }
