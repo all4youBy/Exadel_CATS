9a6b55150da112ebefaaf762ec22ef2a80ecb7dc
Alexandre Adereyko <aadereiko@gmail.com>
2018-07-18 13:18:09 +0300

    clear Redux Registration page

--- front-end/src/Scenes/Registration/Services/Reducers/students.js
+++ front-end/src/Scenes/Registration/Services/Reducers/students.js
@@ -1,16 +1,6 @@
 import * as types from '../Actions/types';
 
-const initialState = [
-  {
-    email: 'pupa@gmail.com',
-    theFirstName: 'Вася',
-    theSecondName: 'Пупкин',
-    institution: 'БГУ',
-    faculty: 'ФПМИ',
-    yearTermination: 2020,
-    primarySkill: '',
-  },
-];
+const initialState = [];
 
 const registrationStudentReducer = (state = initialState, action) => {
   switch (action.type) {
