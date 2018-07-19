9af63120e37f863630495551e9749e2076a6beed
SkoriyVladislav <Skoriy.97@gmail.com>
2018-07-19 17:13:10 +0300

    Return mail module

--- back-end/src/main/java/com/exadel/team3/backend/services/task/task_run/TaskRunner.java
+++
@@ -1,8 +0,0 @@
-package com.exadel.team3.backend.services.task.task_run;
-
-import java.util.List;
-
-public interface TaskRunner {
-    boolean runTask(List<Class<?>> classList, String... args);
-
-}
