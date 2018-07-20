d698d954f08df756c690e277b6fcab3c18e23b06
Alexandre Adereyko <aadereiko@gmail.com>
2018-07-20 12:24:51 +0300

    installing react-router-redux

--- front-end/src/Services/reducers.js
+++ front-end/src/Services/reducers.js
@@ -1,13 +1,15 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
 import loadMainProject from '../Main/Services/Reducers/load';
 import students from '../Scenes/Registration/Services/Reducers/students';
 import teachers from '../Scenes/Registration/Services/Reducers/teachers';
-
 
 const reducers = combineReducers({
   loadMainProject,
   students,
   teachers,
+  routing: routerReducer,
 });
 
 export default reducers;
