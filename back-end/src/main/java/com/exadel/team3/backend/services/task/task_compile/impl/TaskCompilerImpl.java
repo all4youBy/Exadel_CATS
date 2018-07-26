4e58ad86791aad76a68e8f9a269b92fa513cf516
SkoriyVladislav <Skoriy.97@gmail.com>
2018-07-26 13:48:41 +0300

    new Task Compiler Impl and new Task Runner

--- back-end/src/main/java/com/exadel/team3/backend/services/task/task_compile/impl/TaskCompilerImpl.java
+++ back-end/src/main/java/com/exadel/team3/backend/services/task/task_compile/impl/TaskCompilerImpl.java
@@ -1,11 +1,9 @@
 package com.exadel.team3.backend.services.task.task_compile.impl;
 
-import com.exadel.team3.backend.services.SolutionService;
 import com.exadel.team3.backend.services.task.task_compile.TaskCompiler;
 import org.mdkt.compiler.InMemoryJavaCompiler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
