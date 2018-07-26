3e8abaddc9e0978caedf2709442c4e288d5a55ea
SkoriyVladislav <Skoriy.97@gmail.com>
2018-07-26 14:08:33 +0300

    A new way to run a user application

--- back-end/src/main/java/com/exadel/team3/backend/services/task/task_run/impl/TaskRunnerImpl.java
+++ back-end/src/main/java/com/exadel/team3/backend/services/task/task_run/impl/TaskRunnerImpl.java
@@ -1,12 +1,10 @@
 package com.exadel.team3.backend.services.task.task_run.impl;
 
-import com.exadel.team3.backend.services.task.task_run.CustomClassLoader;
 import com.exadel.team3.backend.services.task.task_run.TaskRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
@@ -17,34 +15,23 @@ public class TaskRunnerImpl implements TaskRunner {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
-    public boolean runTask(List<Class<?>> classList, String... args) {
+    public String runTask(List<Class<?>> classList, String[] args) {
 
-        // create the custom class loader
-        ClassLoader classLoader = new CustomClassLoader();
-        Method main = null;
-
+        Method execute = null;
         for (Class<?> clazz : classList) {
             try {
-
-                // get the main method
-                main = clazz.getMethod("main", args.getClass());
+                execute = clazz.getMethod("execute", args.getClass());
             } catch (NoSuchMethodException ex) {
-                logger.info("did not find the \"main\" method in this class. " + ex.getMessage());
+                logger.info("did not find the \"execute\" method in this class. " + ex.getMessage());
             }
         }
-
+        String executeReturn = null;
         try {
-            main.invoke(null, (Object) args);
+            executeReturn = (String) execute.invoke(null, (Object) args);
         } catch (IllegalAccessException | InvocationTargetException ex) {
             logger.error("Can not run file" + ex.getMessage());
         }
 
-        //TODO It is necessary to write this file into the database
-        //new File(getClass().getClassLoader().getResource(nameOfOutputFile).getFile());
-
-
-        //TODO Ask the database if there is such a file
-        // return new File(getClass().getClassLoader().getResource(nameOfOutputFile).getFile()).exists();
-        return true;
+        return executeReturn;
     }
 }
