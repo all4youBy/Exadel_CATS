25aff84d274c28960a848b8fb0431b2c3c41652f
SkoriyVladislav <Skoriy.97@gmail.com>
2018-07-30 13:44:52 +0300

    initial commit

--- back-end/src/main/java/com/exadel/team3/backend/controllers/TaskController.java
+++ back-end/src/main/java/com/exadel/team3/backend/controllers/TaskController.java
@@ -1,6 +1,9 @@
 package com.exadel.team3.backend.controllers;
 
+import com.exadel.team3.backend.controllers.requests.TaskForGroupRequest;
+import com.exadel.team3.backend.entities.Solution;
 import com.exadel.team3.backend.entities.Task;
+import com.exadel.team3.backend.services.SolutionService;
 import com.exadel.team3.backend.services.TaskService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +12,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/task")
 public class TaskController {
     @Autowired
     TaskService taskService;
+    @Autowired
+    SolutionService solutionService;
 
     @PostMapping("/add")
     public ResponseEntity<?> addTask(@RequestBody Task task){
@@ -32,5 +39,33 @@ public class TaskController {
     @DeleteMapping
     public void deleteTask(@RequestBody Task task){
         taskService.deleteItem(task);
+    }
+
+    @PostMapping("/solution")
+    public ResponseEntity<?> addFilesInSolution(@RequestParam("files") MultipartFile... files) {
+        for (MultipartFile file : files) {
+
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body("Files added");
+    }
+
+    @PostMapping("/group")
+    public ResponseEntity<?> assignTask(@RequestBody TaskForGroupRequest taskRequest) {
+        List<Solution> taskForGroup = solutionService.assignSolutionToGroup(
+                taskRequest.getId(),
+                taskRequest.getGroup(),
+                taskRequest.getStart(),
+                taskRequest.getDeadline(),
+                taskRequest.getAssignedBy());
+        if(taskForGroup == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Can't generate tests for group.");
+        }
+
+        return new ResponseEntity<>(taskForGroup, HttpStatus.OK);
+    }
+
+    @GetMapping("/tasks")
+    public List<Task> getTasks() {
+        return taskService.getItems();
     }
 }
---
+++ back-end/src/main/java/com/exadel/team3/backend/controllers/requests/TaskForGroupRequest.java
@@ -0,0 +1,26 @@
+package com.exadel.team3.backend.controllers.requests;
+
+import lombok.Data;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+@Data
+public class TaskForGroupRequest {
+
+    @NonNull
+    private String group;
+
+    @NonNull
+    private ObjectId id;
+
+    @NonNull
+    private String title;
+    private LocalDateTime start;
+    private LocalDateTime deadline;
+    private Collection<ObjectId> topicsId;
+    @NonNull
+    private String assignedBy;
+}
