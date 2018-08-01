4e50e32275db0670fa8f443f0ba24a68f912b00a
Stepan Miakchilo <stepan.miakchilo@gmail.com>
2018-08-01 14:58:36 +0300

    Created RatingProjection class for retrieving aggregated rating information and UserRatingDTO class Corrected @Repository annotations (was made back then but overwritten for some reason)

---
+++ back-end/src/main/java/com/exadel/team3/backend/dto/UserRatingDTO.java
@@ -0,0 +1,18 @@
+package com.exadel.team3.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+@AllArgsConstructor
+public class UserRatingDTO {
+    @NonNull
+    private String firstName;
+
+    @NonNull
+    private String lastName;
+
+    private int rating;
+
+}
